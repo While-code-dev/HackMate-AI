56cfc78b2c1444fba39e1cc430bef3a9ace9c5fd
BADIGER YETESH <yeteshbadiger@gmail.com>
2026-08-28 16:22:41 +0530

    Fix production backend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://hackmate-ai.onrender.com/api",
   timeout: 120000,
   headers: {
     "Content-Type": "application/json",
@@ -10,9 +10,11 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
